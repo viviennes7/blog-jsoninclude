366965cf97a66763e0a69f2a98ee0fbe3a31789f
minsoo <viviennes7@adwitt.com>
2018-05-23 22:02:17 +0900

    init commit

--- src/test/java/com/ms/jsoninclude/JsonTest.java
+++ src/test/java/com/ms/jsoninclude/JsonTest.java
@@ -11,8 +11,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 
 import static java.util.Arrays.asList;
